7d26525ffeec9553eed59df5c07715a7dfa35b56
Reeshul <reeshul@hotmail.com>
2021-03-08 14:37:16 +0000

    prints a balance of 3000 if a deposit of 2000 is made

--- BankStatement.js
+++ BankStatement.js
@@ -1,6 +1,14 @@
 class BankStatement {
   constructor() {
+    this.balance = 1000;
     this.statement = "";
+  }
+
+  deposit(amount, dateArray) {
+    this.balance += amount;
+    this.statement += `${convertDateToString(dateArray)} || ${amount.toFixed(
+      2
+    )} || || ${this.balance.toFixed(2)}\n`;
   }
 
   printStatement() {
@@ -10,6 +18,14 @@ class BankStatement {
       "10/01/2012 || 1000.00 || || 1000.00"
     );
   }
+}
+
+function dateArrayToString([DD, MM, YYYY]) {
+  return `${DD}/${MM}/${YYYY}`;
+}
+
+function convertDateToString([Day, Month, Year]) {
+  return new Date(Year, Month - 1, Day).toLocaleDateString("en-GB");
 }
 
 module.exports = BankStatement;
--- BankStatement.test.js
+++ BankStatement.test.js
@@ -11,4 +11,12 @@ describe("#printStatament", () => {
       "date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00"
     );
   });
+  it("prints a balance of 3000 if a deposit of 2000 is made", () => {
+    let depositAmount = 2000;
+    let d = [13, 1, 2012];
+    newStatement.deposit(depositAmount, d);
+    expect(newStatement.printStatement()).toEqual(
+      "date || credit || debit || balance\n13/01/2012 || 2000.00 || || 3000.00\n10/01/2012 || 1000.00 || || 1000.00"
+    );
+  });
 });
