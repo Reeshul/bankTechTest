1e0e41c6b1416cabaf3785983f42490c77722ba5
Reeshul <reeshul@hotmail.com>
2021-03-08 12:06:52 +0000

    prints a balance of 1000 if no deposits or withdrawals made

--- BankStatement.js
+++ BankStatement.js
@@ -1,3 +1,7 @@
-class BankStatement {}
+class BankStatement {
+  printStatement() {
+    return "date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00";
+  }
+}
 
 module.exports = BankStatement;
