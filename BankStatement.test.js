8ff2f752093b761aac272d683773811379196e55
Reeshul <reeshul@hotmail.com>
2021-03-08 15:56:11 +0000

    unit test for deposit

--- BankStatement.test.js
+++ BankStatement.test.js
@@ -8,36 +8,51 @@ let depositDate;
 let withdrawalAmount;
 let withdrawalDate;
 
-describe("#printStatament", () => {
+describe("Bank Statement", () => {
   beforeEach(() => {
     newStatement = new BankStatement();
-    depositAmount = 2000;
-    depositDate = [13, 1, 2012];
-    withdrawalAmount = 500;
-    withdrawalDate = [14, 1, 2012];
   });
-  it("prints a balance of 1000 if no deposits or withdrawals made", () => {
-    expect(newStatement.printStatement()).toEqual(
-      "date || credit || debit || balance\n" +
-        "10/01/2012 || 1000.00 || || 1000.00"
-    );
+  describe("#printStatament", () => {
+    beforeEach(() => {
+      depositAmount = 2000;
+      depositDate = [13, 1, 2012];
+      withdrawalAmount = 500;
+      withdrawalDate = [14, 1, 2012];
+    });
+    it("prints a balance of 1000 if no deposits or withdrawals made", () => {
+      expect(newStatement.printStatement()).toEqual(
+        "date || credit || debit || balance\n" +
+          "10/01/2012 || 1000.00 || || 1000.00"
+      );
+    });
+    it("prints a balance of 3000 if a deposit of 2000 is made", () => {
+      newStatement.deposit(depositAmount, depositDate);
+      expect(newStatement.printStatement()).toEqual(
+        "date || credit || debit || balance\n" +
+          "13/01/2012 || 2000.00 || || 3000.00\n" +
+          "10/01/2012 || 1000.00 || || 1000.00"
+      );
+    });
+    it("prints a balance of 2500 if a deposit of 2000 is made followed by a 500 withdrawal", () => {
+      newStatement.deposit(depositAmount, depositDate);
+      newStatement.withdraw(withdrawalAmount, withdrawalDate);
+      expect(newStatement.printStatement()).toEqual(
+        "date || credit || debit || balance\n" +
+          "14/01/2012 || || 500.00 || 2500.00\n" +
+          "13/01/2012 || 2000.00 || || 3000.00\n" +
+          "10/01/2012 || 1000.00 || || 1000.00"
+      );
+    });
   });
-  it("prints a balance of 3000 if a deposit of 2000 is made", () => {
-    newStatement.deposit(depositAmount, depositDate);
-    expect(newStatement.printStatement()).toEqual(
-      "date || credit || debit || balance\n" +
-        "13/01/2012 || 2000.00 || || 3000.00\n" +
-        "10/01/2012 || 1000.00 || || 1000.00"
-    );
-  });
-  it("prints a balance of 2500 if a deposit of 2000 is made followed by a 500 withdrawal", () => {
-    newStatement.deposit(depositAmount, depositDate);
-    newStatement.withdraw(withdrawalAmount, withdrawalDate);
-    expect(newStatement.printStatement()).toEqual(
-      "date || credit || debit || balance\n" +
-        "14/01/2012 || || 500.00 || 2500.00\n" +
-        "13/01/2012 || 2000.00 || || 3000.00\n" +
-        "10/01/2012 || 1000.00 || || 1000.00"
-    );
+
+  describe("#deposit", () => {
+    beforeEach(() => {
+      depositAmount = 333;
+      depositDate = [8, 3, 2021];
+    });
+    it("balance updates to 1333 if 333 is deposited", () => {
+      newStatement.deposit(depositAmount, depositDate);
+      expect(newStatement.balance).toEqual(1333);
+    });
   });
 });
--- helperFunctions.js
+++
@@ -1,4 +0,0 @@
-export const dateArrayToString = ([Day, Month, Year]) =>
-  new Date(Year, Month - 1, Day).toLocaleDateString("en-GB");
-
-module.exports = helperFunctions;
