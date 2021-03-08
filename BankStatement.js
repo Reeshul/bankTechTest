9bdc51f7dd083a3113be454886b2bbdc45a01726
Reeshul <reeshul@hotmail.com>
2021-03-08 11:45:42 +0000

    setting up jest

---
+++ BankStatement.js
@@ -0,0 +1,5 @@
+function BankStatement(a, b) {
+  return a + b;
+}
+
+module.exports = BankStatement;
---
+++ BankStatement.test.js
@@ -0,0 +1,5 @@
+const BankStatement = require("./BankStatement");
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(BankStatement(1, 2)).toBe(3);
+});
