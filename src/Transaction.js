e68f2f3aa1f27c04e6b7621f030de343cc1abf4e
Reeshul <reeshul@hotmail.com>
2021-03-11 23:35:15 +0000

    removed the class keyword

--- src/Transaction.js
+++ src/Transaction.js
@@ -1,10 +1,17 @@
-class Transaction {
-  constructor(date, type, amount, balance) {
-    this.date = date;
-    this.type = type;
-    this.amount = amount;
-    this.balance = balance;
-  }
+// class Transaction {
+//   constructor(date, type, amount, balance) {
+//     this.date = date;
+//     this.type = type;
+//     this.amount = amount;
+//     this.balance = balance;
+//   }
+// }
+
+function Transaction(date, type, amount, balance) {
+  this.date = date;
+  this.type = type;
+  this.amount = amount;
+  this.balance = balance;
 }
 
 module.exports = Transaction;
