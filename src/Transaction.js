05394a06814eb7b773b703a0beff9cd01298458d
Reeshul <reeshul@hotmail.com>
2021-03-12 00:02:58 +0000

    removed commented out class declaration in transaction.js

--- src/Transaction.js
+++ src/Transaction.js
@@ -1,12 +1,3 @@
-// class Transaction {
-//   constructor(date, type, amount, balance) {
-//     this.date = date;
-//     this.type = type;
-//     this.amount = amount;
-//     this.balance = balance;
-//   }
-// }
-
 function Transaction(date, type, amount, balance) {
   this.date = date;
   this.type = type;
